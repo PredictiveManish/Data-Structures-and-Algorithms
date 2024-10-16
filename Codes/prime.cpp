9e7e86eaa5b83c528c3d2e2bfae51a4dce9d761f
PredictiveManish <manisht0914@gmail.com>
2024-10-17 00:29:12 +0530

    Updates

--- Codes/prime.cpp
+++ Codes/prime.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
     int n;
     cin>>n;
     bool flag=0;
-    for(int i=2;i<n;i++){
+    for(int i=2;i<=sqrt(n);i++){
         if(n%i==0){
             cout<<"Non-Prime"<<endl;
             flag=1;
