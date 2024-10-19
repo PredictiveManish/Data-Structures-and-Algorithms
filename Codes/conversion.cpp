07ea8938e315a8b52cd371d9e6d5630c21737352
PredictiveManish <manisht0914@gmail.com>
2024-10-19 23:31:43 +0530

    5 conversions

--- Codes/conversion.cpp
+++ Codes/conversion.cpp
@@ -13,12 +13,63 @@ int binaryToDecimal(int num){
 
     }   
     return ans;
+}
+int decimalToBinary(int num){
+    int ans=0;
+    int value =1;
+    while(num>0){
+        int remainder = num%2;
+        ans +=remainder*value;
+        num /=2;
+        value *=10;
+        
+    }
+    return ans;
+}
+int decimalToOctal(int num){
+    int ans =0;
+    int x=1;
+    while(num>0){
+        int y=num%8;
+        ans+=x*y;
+        num/=8;
+        x*=10;
+    }
+    return ans;
+    
 
+}
+int octalToDecimal(int num){
+    int ans=0;
+    int x=1;
+     while(num>0){
+        int y= num%10;
+        ans +=x*y;
+        x*=8;
+        num/=10;
+
+    }
+    return ans;
+}
+int decimalToHexadecimal(int num){
+    int ans =0;
+    int x=1;
+    while(num>0){
+        int remainder=num%16;
+        ans +=remainder*x;
+        num/=16;
+        x*=10;
+    }
+    return ans;
 }
 int main(){
     int n;
     cin>>n;
 
-    cout<<binaryToDecimal(n)<<endl;
-
+    cout<<"Binary to decimal: "<<binaryToDecimal(n)<<endl;
+    cout<<"Decimal to binary: "<<decimalToBinary(n)<<endl;
+    cout<<"Decimal to Octal: "<<decimalToOctal(n)<<endl;
+    cout<<"Octal to Decimal: "<<octalToDecimal(n)<<endl;
+    cout<<"Decimal to Hexadecimal: "<<decimalToHexadecimal(n)<<endl;
+    return 0;
 }
